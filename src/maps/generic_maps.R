09db4d5bd721641aee25de246752dfd0d846cc51
Jamie Hogg <jamie-hogg@hotmail.com>
2023-08-01 15:00:04 +1000

    Added population map

---
+++ src/maps/generic_maps.R
@@ -0,0 +1,136 @@
+## -----------------------------------------------------------------------------
+## CHD maps ## -----------------------------------------------------------------
+## -----------------------------------------------------------------------------
+
+source("src/ms.R")
+
+## -----------------------------------------------------------------------------
+
+# Select temporary dataset
+df_temp <- lga$pop %>% 
+  group_by(Year, LGA_Code) %>% 
+  summarise(N = sum(N),
+            .groups = "drop") %>% 
+  group_by(Year) %>% 
+  mutate(N_c = 2*cut_number(N, 50, labels = FALSE)) %>% 
+  ungroup() %>% 
+  filter(Year %in% c(2015:2019))
+
+# create map data list
+map_temp <- left_join(df_temp,map, by = c("LGA_Code" = "LGA_CODE16")) %>% 
+  st_as_sf() %>%
+  st_transform(4326)
+
+## Create map - 2017 only ## ---------------------------------------------------
+
+# base map - no legend
+base <- map_temp %>% 
+  filter(Year == "2017") %>% 
+  ggplot(aes(fill = N_c))+
+  theme_void()+
+  geom_sf(col = "grey", size = 0.1)+
+  geom_sf(data = wa_border, aes(geometry = geometry), 
+          colour = "black", fill = NA, size = 0.2)+
+  scale_fill_viridis_c(begin = 0.5, end = 1, 
+                       direction = -1,
+                       option = "A")+
+  theme(legend.position = "none",
+        text = element_text(size = 10),
+        plot.title = element_text(margin = margin(0,0,2,0)),
+        plot.margin = unit(c(1,1,1,1), "mm"))
+
+# base map_with legend
+temp <- base +
+  labs(fill = "Population (percentiles)")+
+  guides(fill = guide_colourbar(barwidth = 15, 
+                                title.position = "top",
+                                title.hjust = 0.5))+
+  theme(legend.position = "bottom")
+base_legend <- get_legend(temp)
+
+# Base map with boxes
+#base_boxes <- base_legend + addBoxLabel(1, color = "green", size = 0.2)
+base_boxes <- base + addBoxLabel(1, color = "black", size = 0.2)
+
+# Create list of insets
+perth_inset <- base +
+  xlim(lims$xmin[1], lims$xmax[1]) +
+  ylim(lims$ymin[1], lims$ymax[1]) +
+  theme(panel.border = element_rect(colour = "black", size=0.2, fill=NA),
+        plot.title = element_text(margin = margin(0,0,2,0)),
+        plot.margin = unit(c(1,1,1,1), "mm"))
+
+# create full map
+lay <- rbind(c(2,1),
+             c(1,1),
+             c(3,3))
+out = arrangeGrob(grobs = list(base_boxes, perth_inset, base_legend), 
+                  layout_matrix  = lay,
+                  top = textGrob("2017",gp=gpar(fontsize=10)))
+jsave(plot = out, filename = "map_population2017.png", base_folder = "plts", square = F)
+
+## Create map - all years ## ---------------------------------------------------
+
+year_plt_list <- list()
+seq_years <- unique(df_temp$Year)
+
+for(t in 1:length(seq_years)){
+  
+  # base map - no legend
+  base <- map_temp %>% 
+    filter(Year == seq_years[t]) %>% 
+    ggplot(aes(fill = N_c))+
+    theme_void()+
+    geom_sf(col = "grey", size = 0.1)+
+    geom_sf(data = wa_border, aes(geometry = geometry), 
+            colour = "black", fill = NA, size = 0.2)+
+    scale_fill_viridis_c(begin = 0, end = 1, 
+                         direction = -1,
+                         option = "A")+
+    theme(legend.position = "none",
+          text = element_text(size = 10),
+          plot.title = element_text(margin = margin(0,0,2,0)),
+          plot.margin = unit(c(1,1,1,1), "mm"))
+  
+  # base map_with legend
+  temp <- base +
+    labs(fill = "Population (percentiles)")+
+    guides(fill = guide_colourbar(barwidth = 15, 
+                                  title.position = "top",
+                                  title.hjust = 0.5))+
+    theme(legend.position = "bottom")
+  base_legend <- get_legend(temp)
+  
+  # Base map with boxes
+  #base_boxes <- base_legend + addBoxLabel(1, color = "green", size = 0.2)
+  base_boxes <- base + addBoxLabel(1, color = "black", size = 0.2)
+  
+  # Create list of insets
+  perth_inset <- base +
+    xlim(lims$xmin[1], lims$xmax[1]) +
+    ylim(lims$ymin[1], lims$ymax[1]) +
+    theme(panel.border = element_rect(colour = "black", size=0.2, fill=NA),
+          plot.title = element_text(margin = margin(0,0,2,0)),
+          plot.margin = unit(c(1,1,1,1), "mm"))
+  
+  # create full map
+  lay <- rbind(c(2,1),
+               c(1,1))
+  year_plt_list[[t]] = arrangeGrob(grobs = list(base_boxes, perth_inset), 
+                                   layout_matrix  = lay,
+                                   top = textGrob(seq_years[t],gp=gpar(fontsize=10)))
+  
+  # Progress 
+  message("---- Year: ", seq_years[t])
+  
+}
+
+# Final plot
+lay <- rbind(c(1,2,3),
+             c(4,5,6),
+             c(7,7,7))
+full_inset_plt <- arrangeGrob(grobs = c(year_plt_list, list(base_legend)), layout_matrix  = lay)
+
+jsave(plot = full_inset_plt, filename = "map_population.png", base_folder = "plts", square = F)
+
+## END SCRIPT ## --------------------------------------------------------------
