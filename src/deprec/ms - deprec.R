663af871f7f64505758b03945985cf5b9e840302
Jamie Hogg <jamie-hogg@hotmail.com>
2023-08-17 21:43:11 +1000

    MAJOR: Removed old load files from ms

--- src/deprec/ms - deprec.R
+++ src/deprec/ms - deprec.R
@@ -76,6 +76,85 @@ seifa_ra <- lga$seifa_ra %>%
          ra = as.factor(RA_Name),
          ra = fct_relevel(ra, "Major Cities"))
 
+## Six year YLLs ## ------------------------------------------------------------
+files_to_load = list.files("data/CHDand Asthma YLL results by 6 year ASYLL20230707084532", 
+                           pattern = "*.csv", full.names = T)
+ASYLL6y_list = lapply(files_to_load, read.csv)
+names(ASYLL6y_list) <- c("Asthma_ASYLL_Female",
+                         "Asthma_ASYLL_Male",
+                         "CHD_ASYLL_Female",
+                         "CHD_ASYLL_Male",
+                         "CHD_ASYLL_Persons")
+
+## Load CHD YLLs ## ------------------------------------------------------------
+files_to_load = list.files("data/YLLoutputs20CHD20230614013642", 
+                           pattern = "*.csv", full.names = T)
+CHDYLL_list = lapply(files_to_load, read.csv)
+names(CHDYLL_list) <- c("CHD_ASYLL_Female",
+                        "CHD_YLL_Female",
+                        "CHD_ASYLL_Male",
+                        "CHD_YLL_Male",
+                        "CHD_ASYLL_Persons",
+                        "CHD_YLL_Persons")
+
+## Load Asthma YLLs ## ---------------------------------------------------------
+files_to_load = list.files("data/YLLsfor%20asthma20230619125714", 
+                           pattern = "*.csv", full.names = T)
+ATHYLL_list = lapply(files_to_load, read.csv)
+names(ATHYLL_list) <- c("Asthma_ASYLL_Female",
+                        "Asthma_YLL_Female",
+                        "Asthma_ASYLL_Male",
+                        "Asthma_YLL_Male",
+                        "Asthma_ASYLL_Persons",
+                        "Asthma_YLL_Persons")
+
+## Load Asthma YLDs ## ---------------------------------------------------------
+files_to_load = list.files("data/wMrPResults LGA YLD asthma20230704021943", pattern = "*.xlsx", full.names = T)
+ATHYLD_list = lapply(files_to_load, readxl::read_xlsx)
+# ASYLD with all sex categories
+ATHYLD_list[[1]] <- bind_rows(mutate(ATHYLD_list[[1]], sex = "Persons"), ATHYLD_list[[2]])
+# YLD with all sex categories
+ATHYLD_list[[3]] <- bind_rows(mutate(ATHYLD_list[[3]], sex = "Persons"), ATHYLD_list[[4]])
+# Prev with all sex categories
+ATHYLD_list[[5]] <- bind_rows(mutate(ATHYLD_list[[5]], sex = "Persons"), ATHYLD_list[[6]])
+# set 4-6 to null
+ATHYLD_list[c(2,4,6)] <- NULL
+# rename
+names(ATHYLD_list) <- c("Asthma_ASYLD",
+                        "Asthma_YLD",
+                        "Asthma_Prev")
+# fix columns for Asthma_Prev
+colnames(ATHYLD_list$Asthma_Prev) <- str_replace(colnames(ATHYLD_list$Asthma_Prev), 
+                                                 "mrp_", "")
+# split into Persons, Male and Female
+x1 <- split(ATHYLD_list$Asthma_ASYLD,ATHYLD_list$Asthma_ASYLD$sex)
+names(x1) <- paste0("Asthma_ASYLD_", names(x1))
+x2 <- split(ATHYLD_list$Asthma_YLD,ATHYLD_list$Asthma_YLD$sex)
+names(x2) <- paste0("Asthma_YLD_", names(x2))
+x3 <- split(ATHYLD_list$Asthma_Prev,ATHYLD_list$Asthma_Prev$sex)
+names(x3) <- paste0("Asthma_Prev_", names(x3))
+ATHYLD_list <- c(x1, x2, x3); rm(x1, x2, x3)
+
+## UPDATED ASTHMA YLL ## -------------------------------------------------------
+files_to_load <- list.files("data/UpdatedAsthma YLL Results20230802064257",
+pattern = "*.csv", full.names = T)
+ATHYLL2_list <- lapply(files_to_load, read.csv)
+
+# MUST USE THESE FILES
+
+## UPDATED ASTHMA YLD - 0308 ## ------------------------------------------------
+
+Asthma_ASYLD_Persons <- read_excel("data/WMrPST_results_LGA_ASYLD_converted_currast_MT_6yr.xlsx")
+
+## UPDATED CHD files - 0308 ## -------------------------------------------------
+
+files_to_load <- list.files("data/CHD",
+                            pattern = "*.csv", full.names = T)
+CHDYLD_list <- lapply(files_to_load, read.csv)
+names(CHDYLD_list) <- c("CHD_ASYLD",
+                        "CHD_ASYLD_non6",
+                        "CHD_YLD")
+
 ## Updated Asthma files - 1408 ## ----------------------------------------------
 
 loadRData <- function(fileName){
@@ -126,6 +205,20 @@ rawfiles1708 <- list(Asthma_ASYLL = read_csv("data/RawASYLL and ASYLDs2023081709
 ## New CHD - 1708 ## -----------------------------------------------------------
 
 CHD_ASYLD_1708 <- read_csv("data/CHDnew model results20230817093627/YLD_LGA_CHD_ALL_Total ASYLD count table 6year_POPCOR.csv")
+
+## Grand list - deprec ## ------------------------------------------------------
+df_list <- c(CHDYLL_list, ATHYLL_list)
+asyll_list <- df_list[str_detect(names(df_list), "ASYLL")]
+yll_list <- df_list[str_detect(names(df_list), " YLL")]
+
+all_persons_old <- list(CHD = list(YLL = CHDYLL_list$CHD_YLL_Persons,
+                                   ASYLL =ASYLL6y_list$CHD_ASYLL_Persons,
+                                   YLD = list(),
+                                   ASYLD = list()),
+                Asthma = list(YLL = ATHYLL_list$Asthma_YLL_Persons,
+                              ASYLL = ATHYLL_list$Asthma_ASYLL_Persons,
+                              YLD = ATHYLD_list$Asthma_YLD_Persons,
+                              ASYLD = ATHYLD_list$Asthma_ASYLD_Persons))
 
 ## Grand list ## ---------------------------------------------------------------
 
